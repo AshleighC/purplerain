ec1db200ec60d60bf483bc458acd79111e6fb1b7
amytsai <amietsai@gmail.com>
2013-11-23 23:25:57 -0800

    comma

--- background.js
+++ background.js
@@ -1,5 +1,11 @@
+/* background.js */
 function sendMessage(message) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, message);
   });
+
+var current_weather;
+
+function getWeather() {
+ //TODO: asdfasdf
 }
