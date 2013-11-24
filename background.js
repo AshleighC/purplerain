537e11d1b03ed7d973292c7c72ce0ad477707383
amytsai <amietsai@gmail.com>
2013-11-24 00:38:37 -0800

    added appid

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  // TODO: Do stuff with the city name.
-  alert(message.city);
-});
+    // TODO: Do stuff with the city name.
+    alert(message.city);
+    });
 
 function sendMessage(message) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, message);
-  });
+      chrome.tabs.sendMessage(tabs[0].id, message);
+      });
 }
 
 var current_weather;
@@ -16,21 +16,19 @@ var geo_location = new Object();
 
 function getWeather(lat, lon) {
   console.log("hello");
-  var url = "api.openweathermap.org/data/2.5/weather?" + "lat=" + lat +"&lon=" + lon;
+  var url = "api.openweathermap.org/data/2.5/weather?" + "lat=" + lat +"&lon=" + lon + "&APPID=640c99dd9bbca6a64ecfc02325b17fad";
   console.log(url);
   $.get(url, function(data) {
-    console.log("success");
-    console.log("Weather is:" + data["weather"]["main"]);
-    sendMessage(data["weather"]["main"]);
-    });
+      console.log("success");
+      console.log("Weather is:" + data["weather"]["main"]);
+      sendMessage(data["weather"]["main"]);
+      });
 }
 
 function getLocation() {
   navigator.geolocation.getCurrentPosition(function(position) {
-    getWeather(position.coords.latitude, position.coords.longitude);
-    /*alert("Latitude: " + position.coords.latitude +
-        "\nLongitude: " + position.coords.longitude);*/
-  });
+      getWeather(position.coords.latitude, position.coords.longitude);
+      });
 }
-
+console.log("asdFASDF");
 getLocation();
