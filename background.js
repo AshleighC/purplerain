c29061b03607f4fbedc7b60c575b1ae492a6b8a1
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2013-11-24 00:13:20 -0800

    Added code to handle city name option.

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
-/* background.js */
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  // TODO: Do stuff with the city name.
+  alert(message.city);
+});
+
 function sendMessage(message) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, message);
