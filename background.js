3aba69687fa128c6966720eb34d8c184151acaa9
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2013-11-23 23:40:34 -0800

    Added geolocation.

--- background.js
+++ background.js
@@ -3,9 +3,13 @@ function sendMessage(message) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, message);
   });
+}
 
 var current_weather;
 
 function getWeather() {
- //TODO: asdfasdf
+  navigator.geolocation.getCurrentPosition(function(position) {
+    alert("Latitude: " + position.coords.latitude +
+        "\nLongitude: " + position.coords.longitude);
+  });
 }
