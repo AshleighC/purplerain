7ef37406c212b537281cd1862ea70014910bdea7
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2013-11-24 01:16:59 -0800

    Made the sun appear\!

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    // TODO: Do stuff with the city name.
-    alert(message.cityName);
-    });
+  // TODO: Do stuff with the city name.
+  //alert(message.cityName);
+  sendMessage({weather: "sun"});
+});
 
 function sendMessage(message) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
-        console.log(response.farewell);
-        });
-      });
+    chrome.tabs.sendMessage(tabs[0].id, message);
+  });
 }
 
 var current_weather;
--- js/purplerain.js
+++ js/purplerain.js
@@ -1,3 +1,6 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   console.log(message);
+  if (message.weather === "sun") {
+    $('body').append('<div class="sun"></div>')
+  }
 });
